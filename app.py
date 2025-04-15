d02f42a624901e16fb900c7cf95fb57573e55e5d
chi_kolo <chig_ike@yahoo.com>
2025-04-15 04:41:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -611,5 +611,5 @@ if __name__ == '__main__':
     initialize_application()
     
     # Run app
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
